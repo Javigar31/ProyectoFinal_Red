663e8d9daf78a42cfa6431e09535b28332c11765
Javigar31 <115911486+Javigar31@users.noreply.github.com>
2022-10-15 23:38:02 -0500

    Add files via upload

---
+++ co.edu.uniquindio.proyectofinal.red/src/co/edu/uniquindio/proyectofinal/red/exceptions/AdministradorException.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.proyectofinal.red.exceptions;
+
+public class AdministradorException extends Exception {
+
+	public AdministradorException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
