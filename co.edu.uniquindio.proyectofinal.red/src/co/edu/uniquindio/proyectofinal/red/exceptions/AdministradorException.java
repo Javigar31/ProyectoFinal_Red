2d1939a52be22068a9e766f4517a2a06184bf8c9
Javier García <115911486+Javigar31@users.noreply.github.com>
2022-10-16 17:14:19 -0500

    Add files via upload

---
+++ co.edu.uniquindio.proyectofinal.red/src/co/edu/uniquindio/proyectofinal/red/exceptions/AdministradorException.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.proyectofinal.red.exceptions;
+
+public class AdministradorException extends Exception {
+
+	public AdministradorException(String mensaje) {
+		super(mensaje);
+	}
+
+}
