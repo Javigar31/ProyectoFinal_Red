09b7870f8981c98e19baf8f153a2d48c39361238
Javier García <115911486+Javigar31@users.noreply.github.com>
2022-10-16 01:29:27 -0500

    Delete co.edu.uniquindio.proyectofinal.red directory

--- co.edu.uniquindio.proyectofinal.red/src/co/edu/uniquindio/proyectofinal/red/exceptions/VendedorException.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.proyectofinal.red.exceptions;
-
-public class VendedorException extends Exception {
-
-	public VendedorException(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
